53b7cbaf15ba6a909df82fa1abacd875ca750edb
Ayushaj96 <ayushaj007@gmail.com>
2017-12-21 13:55:25 +0530

    sqlite added

--- app/src/main/java/com/example/vasu/aismap/MainActivity.java
+++ app/src/main/java/com/example/vasu/aismap/MainActivity.java
@@ -2,15 +2,44 @@ package com.example.vasu.aismap;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button b1,b2,b3;
+    EditText ed1,ed2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+      b1=(Button)findViewById(R.id.button5);
+        b2=(Button)findViewById(R.id.button6);
+        b3=(Button)findViewById(R.id.button7);
+
+ed1=(EditText)findViewById(R.id.editText);
+        ed2=(EditText)findViewById(R.id.editText2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
 }
-  
