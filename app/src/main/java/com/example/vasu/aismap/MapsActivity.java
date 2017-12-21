bcaf5437b588e8cceac44264a0ac9b8dcde8be2e
Ayushaj96 <ayushaj007@gmail.com>
2017-12-21 11:30:28 +0530

    added key

--- app/src/main/java/com/example/vasu/aismap/MapsActivity.java
+++ app/src/main/java/com/example/vasu/aismap/MapsActivity.java
@@ -60,15 +60,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -86,7 +77,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    public void onConnectionSuspended(int i) {  
 
     }
 
