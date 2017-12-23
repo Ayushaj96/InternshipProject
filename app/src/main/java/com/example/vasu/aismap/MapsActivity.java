400e950423caf117c17336959c808f7327ccd091
Ayushaj96 <ayushaj007@gmail.com>
2017-12-23 13:41:52 +0530

    Merge branch 'master' of https://github.com/androfly/AIS-MAP
    # Conflicts:
    #	app/src/main/java/com/example/vasu/aismap/MapsActivity.java

--- app/src/main/java/com/example/vasu/aismap/MapsActivity.java
+++ app/src/main/java/com/example/vasu/aismap/MapsActivity.java
@@ -1,14 +1,10 @@
 package com.example.vasu.aismap;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.graphics.Color;
 import android.location.Location;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -18,7 +14,6 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.vasu.aismap.Models.ClusteringItem;
-import com.example.vasu.aismap.Models.DirectionJSONParser;
 import com.example.vasu.aismap.Models.GetLinePath;
 import com.example.vasu.aismap.Models.OwnClusterIconRendered;
 import com.google.android.gms.common.ConnectionResult;
@@ -38,28 +33,16 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
 
 import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         LocationListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
-
+    
     private GoogleMap mMap;
     LocationRequest mLocationRequest;
     GoogleApiClient mGoogleApiClient;
