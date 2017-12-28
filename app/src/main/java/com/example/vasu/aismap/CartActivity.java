7db1178cc61b5a88971f69ee696721c152796308
Vasu Gupta <androfly.official@gmail.com>
2017-12-28 19:53:54 +0530

    Integerated Payments at right place

--- app/src/main/java/com/example/vasu/aismap/CartActivity.java
+++ app/src/main/java/com/example/vasu/aismap/CartActivity.java
@@ -1,9 +1,13 @@
 package com.example.vasu.aismap;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,28 +17,46 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.payumoney.core.PayUmoneyConfig;
+import com.payumoney.core.PayUmoneyConstants;
+import com.payumoney.core.PayUmoneySdkInitializer;
+import com.payumoney.core.entity.TransactionResponse;
+import com.payumoney.sdkui.ui.utils.PayUmoneyFlowManager;
+import com.payumoney.sdkui.ui.utils.ResultModel;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CartActivity extends AppCompatActivity {
 
-    String address,machine_serial_no,access,status,company1,company2,type;
+    String fullName,mobile,emailId,username,address,machine_serial_no,access,status,company1,company2,type;
     int company1quantity , company2quantity ;
 
     Spinner companySpinner ;
+
+    SharedPreferences sharedPreferences ;
 
     ImageButton ibIncQuantity , ibDecQuantity ;
     TextView tvCompany,tvType,tvQuantity,tvCost,tvTotalCost ;
     Button btnPay ;
 
-    int cost = 0 ;
+    String productinfo = "" ;
+    int cost = 0 , totalCost = 0;
     int chosenCompany = 0 ;
 
     CardView cardDetails ;
+
+    String merchantId = "5932019" , merchantKey = "EFtpMftw" , merchantSalt = "m4cHTWKqoL"  ;
+    private PayUmoneySdkInitializer.PaymentParam mPaymentParams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        sharedPreferences=getApplicationContext().getSharedPreferences("MyInfo", MODE_PRIVATE);
 
         cardDetails = findViewById(R.id.cardView) ;
 
@@ -51,6 +73,10 @@ public class CartActivity extends AppCompatActivity {
         btnPay = findViewById(R.id.btnPay);
 
         Intent i = getIntent();
+        fullName = sharedPreferences.getString("FullName" , "") ;
+        emailId = sharedPreferences.getString("Email" , "");
+        mobile = sharedPreferences.getString("Mobile" , "");
+        username = sharedPreferences.getString("Username" , "");
         company1 = i.getStringExtra("company1");
         company1quantity = i.getIntExtra("company1quantity" , 0) ;
         company2 = i.getStringExtra("company2");
@@ -70,6 +96,8 @@ public class CartActivity extends AppCompatActivity {
            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                String temp = ""+adapterView.getItemAtPosition(i) ;
                 if (!temp.equalsIgnoreCase("Select a Company")){
+                    productinfo = temp ;
+                    totalCost = 0 ;
                     if (i == 1){
                         chosenCompany = 1 ;
                     }else if(i == 2){
@@ -77,6 +105,7 @@ public class CartActivity extends AppCompatActivity {
                     }
                     setValues(temp);
                 }else {
+                    productinfo = "" ;
                     if (cardDetails.getVisibility() == View.VISIBLE){
                         cardDetails.setVisibility(View.INVISIBLE);
                     }
@@ -96,7 +125,7 @@ public class CartActivity extends AppCompatActivity {
                int temp = Integer.parseInt(tvQuantity.getText().toString());
                if (temp < (chosenCompany == 1 ? company1quantity : company2quantity)){
                    temp += 1 ;
-                   int totalCost = temp*cost ;
+                   totalCost = temp*cost ;
                    tvQuantity.setText(""+temp);
                    tvTotalCost.setText("Total Cost : " + totalCost+"");
                }else {
@@ -111,7 +140,7 @@ public class CartActivity extends AppCompatActivity {
                 int temp = Integer.parseInt(tvQuantity.getText().toString());
                 if (temp > 0){
                     temp -= 1 ;
-                    int totalCost = temp*cost ;
+                    totalCost = temp*cost ;
                     tvQuantity.setText(""+temp);
                     tvTotalCost.setText("Total Cost : " + totalCost+"");
                 }else {
@@ -124,7 +153,11 @@ public class CartActivity extends AppCompatActivity {
         btnPay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (totalCost > 0){
+                    launchPayUMoneyFlow();
+                }else {
+                    Toast.makeText(CartActivity.this, "Total Cost is 0", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -151,8 +184,160 @@ public class CartActivity extends AppCompatActivity {
 
         tvTotalCost.setText("0");
         tvQuantity.setText("0");
-
+        totalCost = 0 ;
 
     }
+
+    private void launchPayUMoneyFlow() {
+
+        PayUmoneyConfig payUmoneyConfig = PayUmoneyConfig.getInstance();
+
+        payUmoneyConfig.setDoneButtonText("Pay Now");
+        payUmoneyConfig.setPayUmoneyActivityTitle("Payment Activity");
+        PayUmoneySdkInitializer.PaymentParam.Builder builder = new PayUmoneySdkInitializer.PaymentParam.Builder();
+
+        double amount = (double) totalCost ;
+        String txnId = System.currentTimeMillis() + "";
+        String phone = mobile;
+        String productName = productinfo;
+        String firstName = fullName;
+        String email = emailId;
+        String udf1 = "";
+        String udf2 = "";
+        String udf3 = "";
+        String udf4 = "";
+        String udf5 = "";
+        String udf6 = "";
+        String udf7 = "";
+        String udf8 = "";
+        String udf9 = "";
+        String udf10 = "";
+
+        builder.setAmount(amount)
+                .setTxnId(txnId)
+                .setPhone(phone)
+                .setProductName(productName)
+                .setFirstName(firstName)
+                .setEmail(email)
+                .setsUrl("https://www.payumoney.com/mobileapp/payumoney/success.php")
+                .setfUrl("https://www.payumoney.com/mobileapp/payumoney/failure.php")
+                .setUdf1(udf1)
+                .setUdf2(udf2)
+                .setUdf3(udf3)
+                .setUdf4(udf4)
+                .setUdf5(udf5)
+                .setUdf6(udf6)
+                .setUdf7(udf7)
+                .setUdf8(udf8)
+                .setUdf9(udf9)
+                .setUdf10(udf10)
+                .setIsDebug(false)
+                .setKey(merchantKey)
+                .setMerchantId(merchantId);
+
+        try {
+            mPaymentParams = builder.build();
+
+            mPaymentParams = calculateServerSideHashAndInitiatePayment1(mPaymentParams);
+
+            PayUmoneyFlowManager.startPayUMoneyFlow(mPaymentParams,CartActivity.this, R.style.AppTheme_default, false);
+
+
+        } catch (Exception e) {
+            // some exception occurred
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static String hashCal(String str) {
+        byte[] hashseq = str.getBytes();
+        StringBuilder hexString = new StringBuilder();
+        try {
+            MessageDigest algorithm = MessageDigest.getInstance("SHA-512");
+            algorithm.reset();
+            algorithm.update(hashseq);
+            byte messageDigest[] = algorithm.digest();
+            for (byte aMessageDigest : messageDigest) {
+                String hex = Integer.toHexString(0xFF & aMessageDigest);
+                if (hex.length() == 1) {
+                    hexString.append("0");
+                }
+                hexString.append(hex);
+            }
+        } catch (NoSuchAlgorithmException ignored) {
+        }
+        return hexString.toString();
+    }
+
+    private PayUmoneySdkInitializer.PaymentParam calculateServerSideHashAndInitiatePayment1(final PayUmoneySdkInitializer.PaymentParam paymentParam) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        HashMap<String, String> params = paymentParam.getParams();
+        stringBuilder.append(params.get(PayUmoneyConstants.KEY) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.TXNID) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.AMOUNT) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.PRODUCT_INFO) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.FIRSTNAME) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.EMAIL) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF1) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF2) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF3) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF4) + "|");
+        stringBuilder.append(params.get(PayUmoneyConstants.UDF5) + "||||||");
+
+        stringBuilder.append(merchantSalt);
+
+        String hash = hashCal(stringBuilder.toString());
+        paymentParam.setMerchantHash(hash);
+
+        return paymentParam;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result Code is -1 send from Payumoney activity
+        Log.d("TRANSACTION", "request code " + requestCode + " resultcode " + resultCode);
+        if (requestCode == PayUmoneyFlowManager.REQUEST_CODE_PAYMENT && resultCode == RESULT_OK && data !=
+                null) {
+            TransactionResponse transactionResponse = data.getParcelableExtra(PayUmoneyFlowManager
+                    .INTENT_EXTRA_TRANSACTION_RESPONSE);
+
+            ResultModel resultModel = data.getParcelableExtra(PayUmoneyFlowManager.ARG_RESULT);
+
+            // Check which object is non-null
+            if (transactionResponse != null && transactionResponse.getPayuResponse() != null) {
+                if (transactionResponse.getTransactionStatus().equals(TransactionResponse.TransactionStatus.SUCCESSFUL)) {
+                    Log.i("TRANSACTION" , "Success");
+                    Toast.makeText(this, "Transaction Success", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.i("TRANSACTION" , "Failure");
+                    Toast.makeText(this, "Transaction Failed", Toast.LENGTH_SHORT).show();
+                }
+
+                // Response from Payumoney
+                String payuResponse = transactionResponse.getPayuResponse();
+
+                // Response from SURl and FURL
+                String merchantResponse = transactionResponse.getTransactionDetails();
+
+                new AlertDialog.Builder(this)
+                        .setCancelable(false)
+                        .setMessage("Payu's Data : " + payuResponse + "\n\n\n Merchant's Data: " + merchantResponse)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                dialog.dismiss();
+                            }
+                        }).show();
+
+            } else if (resultModel != null && resultModel.getError() != null) {
+                Log.d("TRANSACTION", "Error response : " + resultModel.getError().getTransactionResponse());
+            } else {
+                Log.d("TRANSACTION", "Both objects are null!");
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/vasu/aismap/MapsActivity.java
+++ app/src/main/java/com/example/vasu/aismap/MapsActivity.java
@@ -247,10 +247,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         etSearch.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    pDialog.setTitle("Find all Machines near : " + etSearch.getText().toString());
+                    pDialog.show();
                     FindAllSearchMachines fasm = new FindAllSearchMachines(MapsActivity.this, etSearch.getText().toString(), new AsyncResponseFindAllSearches() {
                         @Override
                         public void processFinish(ArrayList<MarkerModel> output) {
                             hideKeyboard(MapsActivity.this);
+                            pDialog.dismissWithAnimation();
                             CustomSearchListAdapter csla = new CustomSearchListAdapter(MapsActivity.this , output);
                             DialogPlus dialog = DialogPlus.newDialog(MapsActivity.this)
                                     .setAdapter(csla)
@@ -349,6 +352,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 zoom  = mMap.getCameraPosition().zoom;
             }
         });
+
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(false);
+
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 16));
 
         if (mCurrentLocation.getLatitude() == 0.0 && mCurrentLocation.getLongitude() == 0.0 ){
@@ -645,17 +652,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         pDialog.dismissWithAnimation();
         LatLng ll = mmAddress.getLatLng() ;
         for (MarkerModel allMM : allShowingMarkers){
-            if (allMM.getAddress().equals(mmAddress.getAddress())) present = true ;
+            if (allMM.getAddress().equals(mmAddress.getAddress())) {
+                present = true ;
+                selectedMarker = allMM.getMarker() ;
+                break;
+            }
         }
         if (!present) {
             Log.i("MARKER" , "HERE");
             m = mMap.addMarker(new MarkerOptions().title("Status : " + (mmAddress.getStatus().equalsIgnoreCase("yes") ? "Working" : "Not Working")).snippet("Quantity : " + (mmAddress.getCompany1quantity()+mmAddress.getCompany2quantity()) ).position(ll).icon(BitmapDescriptorFactory.fromResource(R.drawable.machine)));
             mmAddress.setMarker(m);
             allShowingMarkers.add(mmAddress);
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mmAddress.getLatLng(), 16));
+            mmAddress.getMarker().showInfoWindow() ;
+        }else{
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(selectedMarker.getPosition(), 16));
+            selectedMarker.showInfoWindow() ;
         }
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mmAddress.getLatLng(), 16));
-        mmAddress.getMarker().showInfoWindow() ;
-        selectedMarker = mmAddress.getMarker() ;
+
         Location temp = new Location(LocationManager.GPS_PROVIDER);
         temp.setLatitude(mmAddress.getMarker().getPosition().latitude);
         temp.setLongitude(mmAddress.getMarker().getPosition().longitude);
@@ -693,6 +707,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     .setIcon(android.R.drawable.ic_dialog_alert)
                     .setTitle("Closing Activity")
                     .setMessage("Are you sure you want to close this activity?")
+                    .setIcon(R.drawable.ic_info_outline)
                     .setPositiveButton("Yes", new DialogInterface.OnClickListener()
                     {
                         @Override
